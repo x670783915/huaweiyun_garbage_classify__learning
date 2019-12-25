424e63b7dcfe4cca56854e6ab24e747eced52839
LX <1804307732@qq.com>
2019-12-25 19:43:24 +0800

    update info

--- README.md
+++ README.md
@@ -8,9 +8,30 @@
 
 ### raw dataset
 ![raw_dataset_cnt](./images/raw_dataset_cnt.png)
+![classes](./images/classes.png)
 
+### mydataset
+I used some models to overfit the original training data, and then used those models to distinguish the new additions.
+![ext_data_cnt](./images/pretrain_model_ext.png)
+and the first version of mydataset
+![ext_data_cnt](./images/dataset_v1_cnt.png)
+And then there are a lot of problems with the data set that haven't been solved very well, so I'll offer both the original data set and my data set.
+
+- [raw_dataset(garbage_classify_v2.zip)](https://modelarts-competitions.obs.cn-north-1.myhuaweicloud.com/garbage_classify/dataset/garbage_classify_v2.zip) in official website
+- [mydataset_v1](https://pan.baidu.com/s/1fzTWS5dVhEYHT9pLLZwn9A) in BaiduYun
 
 ## Models
+The model was finally used Se_ResNext101_64x4d pretrained in imagenet.
+
+For better performance, i alos use [CBAM](https://arxiv.org/pdf/1807.06521.pdf
+) and [FocalLoss](https://arxiv.org/pdf/1708.02002.pdf
+) Module to help training.
+
+### TODOS
+- [ ] feature extractor
+- [ ] organize the code
+- [x] test model  
+- ...
 
 ### requirements
 - pytorch 1.3.1 or above
@@ -22,11 +43,14 @@
 
 ## Results
 
-Model | Train Acc | Val Acc
--|-|-
-se_resnext101_32x4d + ce | 0.9825 | 0.9005
-se_resnext101_32x4d + fc | 0.9920 | 0.8986
-se_resnext101_32x4d + cbam + fc | 0.9993 | 0.9032
+Model | Train Acc | Val Acc | Test Acc
+-|-|-|-
+se_resnext101_32x4d + ce | 0.9825 | 0.9005 | 
+se_resnext101_32x4d + fc | 0.9920 | 0.8986 | 
+se_resnext101_32x4d + cbam + fc | 0.9993 | 0.9032 | 0.9005
 
-
+se_resnext101_32x4d + fc
+![focalloss_train](./images/focalloss_fc_train_info.png)
+se_resnext101_32x4d + cbam + fc
+![cbam_focalloss_train](./images/se_resnext101_64x4d_cbam_fl.png)
 
--- test.py
+++ test.py
@@ -12,50 +12,53 @@ from ops import FocalLoss
 from transform import get_test_transform
 from torch.utils.data import DataLoader
 
-best_acc = 0
-start_epoch = 0
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-num_class = 40
-batch_size = 256
-tot_epoch = 30
-lr = 1e-3
+# best_acc = 0
+# start_epoch = 0
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# num_class = 40
+# batch_size = 256
+# tot_epoch = 30
+# lr = 1e-3
 
-# model
-pretrainedmodel = 'se_resnext101_32x4d'
-pretrainedmodel_path = './pretrain_models/...' # 具体设置
-model, _ = se_resnext101_32x4d(40)
-model.to(device)
-# criterion = FocalLoss(num_class).to(device)
-criterion = nn.CrossEntropyLoss().to(device)
+# # model
+# pretrainedmodel = 'se_resnext101_32x4d'
+# pretrainedmodel_path = './pretrain_models/...' # 具体设置
+# model, _ = se_resnext101_32x4d(40)
+# model.to(device)
+# # criterion = FocalLoss(num_class).to(device)
+# criterion = nn.CrossEntropyLoss().to(device)
 
-classes = (
-    '其他垃圾/一次性快餐盒', '其他垃圾/污损塑料', '其他垃圾/烟蒂', '其他垃圾/牙签',
-    '其他垃圾/破碎花盆及碟碗', '其他垃圾/竹筷',
-    '厨余垃圾/剩饭剩菜', '厨余垃圾/大骨头', '厨余垃圾/水果果皮', '厨余垃圾/水果果肉',
-    '厨余垃圾/茶叶渣', '厨余垃圾/菜叶菜根','厨余垃圾/蛋壳','厨余垃圾/鱼骨',
-    '可回收物/充电宝','可回收物/包','可回收物/化妆品瓶','可回收物/塑料玩具',
-    '可回收物/塑料碗盆','可回收物/塑料衣架',
-    '可回收物/快递纸袋','可回收物/插头电线','可回收物/旧衣服',
-    '可回收物/易拉罐','可回收物/枕头','可回收物/毛绒玩具','可回收物/洗发水瓶',
-    '可回收物/玻璃杯','可回收物/皮鞋','可回收物/砧板','可回收物/纸板箱',
-    '可回收物/调料瓶','可回收物/酒瓶','可回收物/金属食品罐','可回收物/锅',
-    '可回收物/食用油桶','可回收物/饮料瓶',
-    '有害垃圾/干电池','有害垃圾/软膏','有害垃圾/过期药物'
-)
-mean, std = [0.4995, 0.4642, 0.4140], [0.2771, 0.2705, 0.2660]
-test_dataset = GarbageDataset(
-    './garbage_dataset_v1/test', 
-    './garbage_dataset_v1/test.csv', 
-    transforms=get_test_transform(mean, std, 224)
-)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+# classes = (
+#     '其他垃圾/一次性快餐盒', '其他垃圾/污损塑料', '其他垃圾/烟蒂', '其他垃圾/牙签',
+#     '其他垃圾/破碎花盆及碟碗', '其他垃圾/竹筷',
+#     '厨余垃圾/剩饭剩菜', '厨余垃圾/大骨头', '厨余垃圾/水果果皮', '厨余垃圾/水果果肉',
+#     '厨余垃圾/茶叶渣', '厨余垃圾/菜叶菜根','厨余垃圾/蛋壳','厨余垃圾/鱼骨',
+#     '可回收物/充电宝','可回收物/包','可回收物/化妆品瓶','可回收物/塑料玩具',
+#     '可回收物/塑料碗盆','可回收物/塑料衣架',
+#     '可回收物/快递纸袋','可回收物/插头电线','可回收物/旧衣服',
+#     '可回收物/易拉罐','可回收物/枕头','可回收物/毛绒玩具','可回收物/洗发水瓶',
+#     '可回收物/玻璃杯','可回收物/皮鞋','可回收物/砧板','可回收物/纸板箱',
+#     '可回收物/调料瓶','可回收物/酒瓶','可回收物/金属食品罐','可回收物/锅',
+#     '可回收物/食用油桶','可回收物/饮料瓶',
+#     '有害垃圾/干电池','有害垃圾/软膏','有害垃圾/过期药物'
+# )
+# mean, std = [0.4995, 0.4642, 0.4140], [0.2771, 0.2705, 0.2660]
+# test_dataset = GarbageDataset(
+#     './garbage_dataset_v1/test', 
+#     './garbage_dataset_v1/test.csv', 
+#     transforms=get_test_transform(mean, std, 224)
+# )
+# test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
 
-class_probs, class_preds, test_acc, test_5 = test_a(test_dataloader, model, criterion, 0, device, None)
-print(class_probs, class_preds, test_acc, test_5)
-state = {}
-state['class_probs'] = class_probs
-state['class_preds'] = class_preds
-state['test_acc'] = test_acc
-state['test_5'] = test_5
-torch.save(state. 'test_out.pth')
+# class_probs, class_preds, test_acc, test_5 = test_a(test_dataloader, model, criterion, 0, device, None)
+# print(class_probs, class_preds, test_acc, test_5)
+# state = {}
+# state['class_probs'] = class_probs
+# state['class_preds'] = class_preds
+# state['test_acc'] = test_acc
+# state['test_5'] = test_5
+# torch.save(state. 'test_out.pth')
+
+testout = torch.load('testoutput.pth')
+print(testout['test_acc'])
